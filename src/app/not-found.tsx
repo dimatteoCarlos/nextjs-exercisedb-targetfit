af861dbbbd239c05b3cdf56a848ca1ddf6444db7
CarlosDR <dimatteoca@gmail.com>
2024-12-16 17:49:33 -0400

    handling not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+// import Link from 'next/link'
+ 
+// export default function NotFound() {
+//   return (
+//     <div className='dark:bg-dark-background '>
+//       <h2>Not Found</h2>
+//       <p>Could not find requested resource</p>
+//       <Link href="/">Return Home</Link>
+//     </div>
+//   )
+// }
+
+// app/not-found.tsx
+import { notFound } from 'next/navigation';
+
+export default function NotFound() {
+  notFound(); // Triggers the "Not Found" page handling
+
+  return (
+    <div>
+      <h1>Oops! Page Not Found</h1>
+    </div>
+  );
+}
